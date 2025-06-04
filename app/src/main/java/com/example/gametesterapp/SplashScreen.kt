2f1a6c64ef01b390421dac723549c62e24d376db
Nhlanhla Cindi <ST10470261@vcconnect.edu.za>
2025-06-04 10:27:05 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/gametesterapp/SplashScreen.kt
@@ -0,0 +1,31 @@
+package com.example.gametesterapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreen: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        findViewById<ImageView>(R.id.splash_logo).setImageResource(R.drawable.splash_screen_logo)
+
+        val homeIntent = Intent(this@SplashScreen, MainActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            //Do some stuff here, like implement deep linking
+            startActivity(homeIntent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
+    }
+
+    companion object {
+        const val SPLASH_TIME_OUT = 2000
+    }
+
+}
